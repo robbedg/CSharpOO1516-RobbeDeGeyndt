9fe9b96b42995690f3e2e102320f5c3b4b5f200f
robbe.degeyndt <robbe.degeyndt@student.odisee.be>
2015-09-27 16:58:49 +0200

    Colors added

--- Opgave01/Opgave01/Form1.Designer.cs
+++ Opgave01/Opgave01/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.buttonBlauw = new System.Windows.Forms.Button();
+            this.buttonResolution = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRed, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonGreen, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonBlauw, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonResolution, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(777, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.Location = new System.Drawing.Point(3, 185);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(75, 20);
+            this.buttonRed.TabIndex = 0;
+            this.buttonRed.Text = "Red";
+            this.buttonRed.UseVisualStyleBackColor = true;
+            this.buttonRed.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(391, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(383, 176);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.Location = new System.Drawing.Point(3, 211);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(75, 20);
+            this.buttonGreen.TabIndex = 3;
+            this.buttonGreen.Text = "Green";
+            this.buttonGreen.UseVisualStyleBackColor = true;
+            this.buttonGreen.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonBlauw
+            // 
+            this.buttonBlauw.Location = new System.Drawing.Point(3, 237);
+            this.buttonBlauw.Name = "buttonBlauw";
+            this.buttonBlauw.Size = new System.Drawing.Size(75, 21);
+            this.buttonBlauw.TabIndex = 4;
+            this.buttonBlauw.Text = "Blauw";
+            this.buttonBlauw.UseVisualStyleBackColor = true;
+            this.buttonBlauw.Click += new System.EventHandler(this.buttonBlauw_Click);
+            // 
+            // buttonResolution
+            // 
+            this.buttonResolution.Location = new System.Drawing.Point(391, 185);
+            this.buttonResolution.Name = "buttonResolution";
+            this.buttonResolution.Size = new System.Drawing.Size(75, 20);
+            this.buttonResolution.TabIndex = 5;
+            this.buttonResolution.Text = "Resolution";
+            this.buttonResolution.UseVisualStyleBackColor = true;
+            this.buttonResolution.Click += new System.EventHandler(this.buttonResolution_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(391, 211);
+            this.trackBar1.Maximum = 8;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 20);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(777, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "ARGB";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.Button buttonBlauw;
+        private System.Windows.Forms.Button buttonResolution;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Opgave01/Opgave01/Form1.cs
+++ Opgave01/Opgave01/Form1.cs
@@ -15,6 +15,92 @@ namespace Opgave01
         public Form1()
         {
             InitializeComponent();
+            trackBar1.Visible = false;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color color = Color.Black;
+
+            byte g = color.G;
+            byte b = color.B;
+
+            Bitmap bmp = new Bitmap(Image.FromFile("TestIMG.jpg"));
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color gotColor = bmp.GetPixel(x, y);
+                    gotColor = Color.FromArgb(gotColor.R, g, b);
+                    bmp.SetPixel(x, y, gotColor);
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(Image.FromFile("TestIMG.jpg"));
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Color color = Color.Black;
+
+            byte r = color.R;
+            byte b = color.B;
+
+            Bitmap bmp = new Bitmap(Image.FromFile("TestIMG.jpg"));
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color gotColor = bmp.GetPixel(x, y);
+                    gotColor = Color.FromArgb(r, gotColor.G, b);
+                    bmp.SetPixel(x, y, gotColor);
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void buttonBlauw_Click(object sender, EventArgs e)
+        {
+            Color color = Color.Black;
+
+            byte r = color.R;
+            byte g = color.G;
+
+            Bitmap bmp = new Bitmap(Image.FromFile("TestIMG.jpg"));
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color gotColor = bmp.GetPixel(x, y);
+                    gotColor = Color.FromArgb(r, g, gotColor.G);
+                    bmp.SetPixel(x, y, gotColor);
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void buttonResolution_Click(object sender, EventArgs e)
+        {
+            trackBar1.Visible = true;
         }
     }
 }
